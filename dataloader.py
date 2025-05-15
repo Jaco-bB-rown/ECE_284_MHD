c188f51f8df8c0d50efeb500b067afac9247adbf
Jacob Brown <102562306+Jaco-bB-rown@users.noreply.github.com>
2025-05-14 21:28:52 -0700

    set up resnet and bounding boxes

--- dataloader.py
+++ dataloader.py
@@ -1,8 +1,9 @@
 import os
 import pandas as pd
-from torchvision.io import read_image, decode_image
+from torchvision.io import decode_image
 from torch.utils.data import Dataset
 from torchvision.ops import masks_to_boxes
+import torchvision.transforms.v2 as v2
 
 class ISICMaskImageDataset(Dataset):
     def __init__(self, dir, name, transform=None, target_transform=None):
@@ -32,4 +33,28 @@ class ISICMaskImageDataset(Dataset):
         target["labels"] = label
         target["masks"] = mask'''
         return image, {'masks': mask,'boxes': boxes, 'labels': label}
-    #label, mask, boxes,
+    
+class ISICClassImageDataset(Dataset):
+    def __init__(self, dir, name, data_aug_type, transform=None, target_transform=None, size = (256,256)):
+        self.img_labels = pd.read_csv(os.path.join(dir, name+"_GroundTruth.csv"))
+        #bounding box labels
+        self.bb_labels = pd.read_csv(os.path.join(dir, "Pred_bb\\"+name+"_bb_"+data_aug_type+".csv"))
+        self.img_dir = os.path.join(dir,"Images\\",name)
+        self.transform = transform
+        self.target_transform = target_transform
+        self.size = (256,256)
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0]+".jpg")
+        image = decode_image(img_path)
+        x1,y1,x2,y2 = (self.bb_labels.iloc[idx,1],self.bb_labels.iloc[idx,2],self.bb_labels.iloc[idx,3],self.bb_labels.iloc[idx,4])
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+            image = v2.functional.resized_crop(image,top=int(y1),left=int(x1),height=int(y2-y1),width=int(x2-x1),size=self.size)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
--- transforms.py
+++ transforms.py
@@ -1,15 +1,19 @@
 from torchvision.models.detection import MaskRCNN_ResNet50_FPN_V2_Weights
 import torchvision.transforms.v2 as v2
-def dataTransforms(data_aug_type):
+def dataTransforms(data_aug_type,size=(256,256), mask=True):
     """
     Perfroms the requested data augmentation
     """
     weights = MaskRCNN_ResNet50_FPN_V2_Weights.DEFAULT
+    if mask:
+        mask_transform = weights.transforms()
+    else: #dont use it
+        mask_transfrom = v2.Lambda(lambda y: y)
     match data_aug_type:
         case "1":
             return v2.Compose(
             [
             weights.transforms(),
-            v2.Resize((256,256)),
+            v2.Resize(size=size),
             ]
             )
